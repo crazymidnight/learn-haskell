d57ef1d4ad2f70f30f255416481d8e96f46da76b
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2024-06-16 23:48:08 +0700

    format code

--- chapter_1/first_prog.hs
+++ chapter_1/first_prog.hs
@@ -1,11 +1,17 @@
 messyMain :: IO ()
 messyMain = do
-    putStrLn "Who's recipient of this message?"
-    recipient <- getLine
-    putStrLn "Book title:"
-    title <- getLine
-    putStrLn "Who's author of message"
-    author <- getLine
-    putStrLn ("Dear " ++ recipient ++ "!\n"
+  putStrLn "Who's recipient of this message?"
+  recipient <- getLine
+  putStrLn "Book title:"
+  title <- getLine
+  putStrLn "Who's author of message"
+  author <- getLine
+  putStrLn
+    ( "Dear "
+        ++ recipient
+        ++ "!\n"
         ++ "Thank you for buying \""
-        ++ title ++ "\"!\nSincerly you,\n" ++ author)
+        ++ title
+        ++ "\"!\nSincerly you,\n"
+        ++ author
+    )
--- chapter_1/first_prog_refactor.hs
+++ chapter_1/first_prog_refactor.hs
@@ -1,11 +1,11 @@
 main = do
-    putStrLn "Who's recipient of this message?"
-    recipient <- getLine
-    putStrLn "Book title:"
-    title <- getLine
-    putStrLn "Who's author of message"
-    author <- getLine
-    putStrLn (createEmail recipient title author)
+  putStrLn "Who's recipient of this message?"
+  recipient <- getLine
+  putStrLn "Book title:"
+  title <- getLine
+  putStrLn "Who's author of message"
+  author <- getLine
+  putStrLn (createEmail recipient title author)
 
 toPart recipient = "Dear " ++ recipient ++ "!\n"
 
@@ -13,6 +13,7 @@ bodyPart bookTitle = "Thank you for buying \"" ++ bookTitle ++ "\"!\n"
 
 fromPart author = "Sincerly you,\n" ++ author
 
-createEmail recipient bookTitle author = toPart recipient ++
-    bodyPart bookTitle ++
-    fromPart author
+createEmail recipient bookTitle author =
+  toPart recipient
+    ++ bodyPart bookTitle
+    ++ fromPart author
