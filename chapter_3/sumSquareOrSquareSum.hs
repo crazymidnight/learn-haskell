e364b0dacbb20746d4e534722ed896138ed81f7e
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2024-06-19 23:59:24 +0700

    add lambdas

--- chapter_3/sumSquareOrSquareSum.hs
+++ chapter_3/sumSquareOrSquareSum.hs
@@ -21,3 +21,10 @@ lambdaSumSquareOrSquareSum x y =
   )
     (x ^ 2 + y ^ 2)
     ((x + y) ^ 2)
+
+letSumSquareOrSquareSum x y =
+  let sumSquare = (x ^ 2 + y ^ 2)
+      squareSum = (x + y) ^ 2
+   in if sumSquare > squareSum
+        then sumSquare
+        else squareSum
